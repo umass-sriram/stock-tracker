f357c439120a354150d08935b245889011188b95
umass-sriram <Sriram_Sethumadhavan@student.uml.edu>
2025-04-28 13:00:17 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -58,7 +58,8 @@ def get_stocks():
         print("get_stocks")
         verify_token(request)
         symbols = ["AAPL", "GOOGL", "TSLA", "MSFT", "AMZN", "NVDA"]
-        data = yf.download(tickers=" ".join(symbols), interval='1wk', end=end, start=start)
+        #data = yf.download(tickers=" ".join(symbols), interval='1wk', end=end, start=start)
+        data = yf.download(tickers=" ".join(symbols), period="1d", interval="1h", group_by='ticker')
         result = {}
         for symbol in symbols:
             if symbol in data:
